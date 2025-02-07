fdd0c626d9feb8d3564d3b708b2a022c0956c472
L6he <158562450+L6he@users.noreply.github.com>
2025-02-07 13:51:37 +0200

    Update StartPage.xaml.cs

--- MauiPlanets/Views/StartPage.xaml.cs
+++ MauiPlanets/Views/StartPage.xaml.cs
@@ -1,3 +1,5 @@
+using Intents;
+
 namespace Views;
 
 public partial class StartPage : ContentPage
@@ -28,6 +30,9 @@ public partial class StartPage : ContentPage
 		parentAnimation.Add(0.6, 0.8, new Animation(v => imgUranus.Opacity = v, 0, 1, Easing.CubicIn));
 		parentAnimation.Add(0.7, 0.9, new Animation(v => imgNeptune.Opacity = v, 0, 1, Easing.CubicIn));
 
-		parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+        //intro box
+        parentAnimation.Add(0.7, 1, new Animation(v => imgIntro.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
 	}
 }
