d35c7d60d3c09ec2eed791592ca54e6a9dc37af0
L6he <158562450+L6he@users.noreply.github.com>
2025-02-07 13:30:46 +0200

    StartPage.xaml.cs changes

--- MauiPlanets/Views/StartPage.xaml.cs
+++ MauiPlanets/Views/StartPage.xaml.cs
@@ -6,4 +6,21 @@ public partial class StartPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+
+		if (this.AnimationIsRunning("TransitionAnimation"))
+		{
+			return;
+		}
+
+		var parentAnimation = new Animation();
+
+		//PlanetsAnimation
+		parentAnimation.Add(0.2, 0.4, new Animation(v => imgEarth.Opacity = v, 0, 1, Easing.CubicIn));
+
+		parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+	}
 }
