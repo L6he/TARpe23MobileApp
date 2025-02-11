63f51865f37e28f47a28187f4705f45b0f62880a
L6he <158562450+L6he@users.noreply.github.com>
2025-02-11 13:05:50 +0200

    Update App.xaml.cs

--- MauiPlanets/App.xaml.cs
+++ MauiPlanets/App.xaml.cs
@@ -11,8 +11,8 @@ namespace MauiPlanets
 {
     public partial class App : Application
     {
-        const int WindowWidth = 450;
-        const int WindowHeight = 875;
+        const int WindowWidth = 540;
+        const int WindowHeight = 1200;
         public App()
         {
             InitializeComponent();
