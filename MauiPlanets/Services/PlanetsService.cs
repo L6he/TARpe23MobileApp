4f0080e41531b10f3b89bfdb8dd55bcf24d811b0
L6he <158562450+L6he@users.noreply.github.com>
2025-02-11 13:37:17 +0200

    Update PlanetsService.cs

--- MauiPlanets/Services/PlanetsService.cs
+++ MauiPlanets/Services/PlanetsService.cs
@@ -8,8 +8,18 @@ namespace Services
         {
             new()
             {
-
-            }
+                Name = "Mercury",
+                Subtitle = "mercury.png",
+                Description = "Mercury is the first planet from the Sun and the smallest in the Solar System. In English, it is named after the ancient Roman god Mercurius (Mercury), god of commerce and communication, and the messenger of the gods.",
+                AccentColorStart = Color.FromArgb("#353535"),
+                AccentColorEnd = Color.FromArgb("#8d9098"),
+                Images = new()
+                {
+                    "",
+                    "",
+                    ""
+                }
+            },
         };
     }
 }
