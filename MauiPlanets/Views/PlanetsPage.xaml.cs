6619ea00d35d85bf3d7c07a5ae104f4d3ce28055
L6he <158562450+L6he@users.noreply.github.com>
2025-02-11 13:50:56 +0200

    Update PlanetsPage.xaml.cs

--- MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Models;
 using Services;
 
 namespace Views;
@@ -18,4 +19,9 @@ public partial class PlanetsPage : ContentPage
 		lstPopularPlanets.ItemsSource = PlanetsService.GetFeaturedPlanets();
 		lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
     }
+
+	async void Planets_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+	{
+		await Navigation.PushAsync(new PlanetsDetailsPage(e.CurrentSelection.First() as Planet));
+	}
 }
